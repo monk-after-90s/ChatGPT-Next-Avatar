12b69546d7b21760d5d3e9a2dde5af06465aa4ce
90houlaoheshang <907333918@qq.com>
2023-09-14 17:49:40 +0800

    记录任务

--- app/components/popup.js
+++ app/components/popup.js
@@ -1,52 +1,57 @@
 export function load_popup() {
-    let isDragging = false;
-    let offsetX, offsetY;
+  let isDragging = false;
+  let offsetX, offsetY;
 
-    document.querySelector('#windowHeader').addEventListener('mousedown', function (e) {
-        isDragging = true;
-        offsetX = e.clientX - document.getElementById('draggableWindow').offsetLeft;
-        offsetY = e.clientY - document.getElementById('draggableWindow').offsetTop;
+  document
+    .querySelector("#windowHeader")
+    .addEventListener("mousedown", function (e) {
+      isDragging = true;
+      offsetX =
+        e.clientX - document.getElementById("draggableWindow").offsetLeft;
+      offsetY =
+        e.clientY - document.getElementById("draggableWindow").offsetTop;
     });
 
-    document.addEventListener('mouseup', function () {
-        isDragging = false;
+  document.addEventListener("mouseup", function () {
+    isDragging = false;
+  });
+
+  document.addEventListener("mousemove", function (e) {
+    if (isDragging) {
+      document.getElementById("draggableWindow").style.left =
+        e.clientX - offsetX + "px";
+      document.getElementById("draggableWindow").style.top =
+        e.clientY - offsetY + "px";
+    }
+  });
+
+  //select下拉
+  let BASE_URL = "http://region-9.autodl.pro:32808"; //fixme
+
+  fetch(`${BASE_URL}/digital_humans`)
+    .then((response) => response.json())
+    .then((data) => {
+      const selectElement = document.getElementById("digman-select");
+      data.forEach((item) => {
+        const optionElement = document.createElement("option");
+        optionElement.text = item.name;
+        optionElement.value = item.value;
+        selectElement.add(optionElement);
+      });
+      selectElement.addEventListener("change", (event) => {
+        // 获取被选定的option元素
+        // let selectedOption = event.target;
+        // prepare_show(selectedOption.value);
+      });
     });
-
-    document.addEventListener('mousemove', function (e) {
-        if (isDragging) {
-            document.getElementById('draggableWindow').style.left = (e.clientX - offsetX) + "px";
-            document.getElementById('draggableWindow').style.top = (e.clientY - offsetY) + "px";
-        }
-    });
-
-
-    //select下拉
-    let BASE_URL = "http://region-9.autodl.pro:32808";
-
-    fetch(`${BASE_URL}/digital_humans`)
-        .then(response => response.json())
-        .then(data => {
-            const selectElement = document.getElementById('digman-select');
-            data.forEach(item => {
-                const optionElement = document.createElement('option');
-                optionElement.text = item.name;
-                optionElement.value = item.value;
-                selectElement.add(optionElement);
-            });
-            selectElement.addEventListener("change", (event) => {
-                // 获取被选定的option元素
-                // let selectedOption = event.target;
-                // prepare_show(selectedOption.value);
-            });
-        });
 }
 
 export function showPopup() {
-    document.getElementById('draggableWindow').style.display = 'block';
-    document.getElementById('minimizedWindow').style.display = 'none';
+  document.getElementById("draggableWindow").style.display = "block";
+  document.getElementById("minimizedWindow").style.display = "none";
 }
 
 export function hidePopup() {
-    document.getElementById('draggableWindow').style.display = 'none';
-    document.getElementById('minimizedWindow').style.display = 'block';
+  document.getElementById("draggableWindow").style.display = "none";
+  document.getElementById("minimizedWindow").style.display = "block";
 }
